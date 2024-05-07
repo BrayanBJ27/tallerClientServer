ec87658f81bf65f216d2d97dcc0a217969d885e9
BrayanBJ27 <xavi_braian@hotmail.com>
2024-05-07 12:27:45 -0500

    feat: :sparkles: Update index.js change reat-dom to react-dom/client

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap aquí
 import App from './App';
 
 const container = document.getElementById('root');
 
-createRoot(container).render(
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
